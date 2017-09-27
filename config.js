a9d92ac1fd03ee3a4fdb14835ca79d2801369750
marknote <marknote@aliyun.com>
2017-09-28 06:16:23 +0800

    init

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    config:function(){
+        return (
+            {
+                adminUsers:[ADMIN_ID], //admin users' telegram id, should be numbers
+                botToken: 'YOUR_BOT_TOEKN', // bot token
+
+            }
+        );
+    }
+};
---
+++ index.js
@@ -0,0 +1,40 @@
+const config = require('./config').config();
+const TeleBot = require('telebot');
+const bot = new TeleBot(config.botToken);
+const { spawn } = require('child_process');
+const adminUsers = config.adminUsers;
+
+bot.on('text', (msg) => {
+    const id = msg.from.id;
+    msg.reply.text('$:'+msg.text);
+    if(adminUsers.indexOf(id) < 0){
+        msg.reply.text('You are not admin!');
+        return;
+    }
+    let words = msg.text.split(" ");
+    let len = words.length;
+    let args = [];
+    if (len > 1 ){
+        args = words.slice(1, len);
+        
+    } 
+    console.log('args:'+args);
+
+    const shell = spawn(words[0],args);
+    
+    shell.stdout.on('data', (data) => {
+        msg.reply.text(`stdout: ${data}`);
+    });
+    
+    shell.stderr.on('data', (data) => {
+        msg.reply.text(`stderr: ${data}`);
+    });
+    
+    shell.on('close', (code) => {
+        msg.reply.text(`shell exited with code ${code}`);
+    });
+
+}
+);
+
+bot.start();
